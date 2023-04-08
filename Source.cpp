d6c99c1f50d73372a9ae9b127189529c191eeeea
Semyon <semen.shilovski@yandex.ru>
2023-04-08 20:06:48 +0300

    refactoring source module

--- Source.cpp
+++ Source.cpp
@@ -5,44 +5,34 @@
 #include <algorithm>
 #include <string>
 
-
-
-
 struct employee {
     int num;
     char name[10];
     double hours;
 };
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cerr << "Error: Argument exception (must be equal 3)" << std::endl;
-        return 1;
-    }
-
+void createProcess(const char* commandLine) {
     STARTUPINFO si; // information about windows(console)
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
     PROCESS_INFORMATION pi; // information about process, for controll it
     ZeroMemory(&pi, sizeof(pi));
 
-    std::string command = "Creator.exe " + std::string(argv[1]) + std::string(" ") + std::string(argv[2]);
-    TCHAR commandLine[MAX_PATH];
-    _tcscpy_s(commandLine, MAX_PATH, command.c_str());
-
-    if (!CreateProcess(NULL, commandLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+    if (!CreateProcess(NULL, (TCHAR*)commandLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
     {
         std::cerr << "Error creating process" << std::endl;
-        return 1;
+        exit(1);
     }
 
     WaitForSingleObject(pi.hProcess, INFINITE);
     CloseHandle(pi.hProcess);
+}
 
-    std::ifstream input(argv[1], std::ios::binary);
+void readEmployeeData(const char* filename) {
+    std::ifstream input(filename, std::ios::binary);
     if (!input) {
         std::cerr << "Error: Could not open file for reading" << std::endl;
-        return 1;
+        exit(1);
     }
 
     employee record;
@@ -54,35 +44,25 @@ int main(int argc, char* argv[]) {
         std::cout << "Hours: " << record.hours << std::endl;
         std::cout << std::endl;
     }
-    
-    std::cout << "Write name for Report file : " << std::endl;
+
+    input.close();
+}
+
+void generateReport(const char* inputFilename) {
     std::string reportFile;
-    std::cin >> reportFile;
     double salary;
+    std::cout << "Write name for Report file : " << std::endl;
+    std::cin >> reportFile;
     std::cout << "Write salary : " << std::endl;
     std::cin >> salary;
-    input.close();
 
-    ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si);
-    ZeroMemory(&pi, sizeof(pi));
+    std::string command = "Reporter.exe " + std::string(inputFilename) + std::string(" ") + reportFile + std::string(" ") + std::to_string(salary);
+    createProcess(command.c_str());
 
-    command = "Reporter.exe " + std::string(argv[1]) + std::string(" ") + reportFile + std::string(" ") + std::to_string(salary);
-    _tcscpy_s(commandLine, MAX_PATH, command.c_str());
-
-    if (!CreateProcess(NULL, commandLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
-    {
-        std::cerr << "Error creating process" << std::endl;
-        return 1;
-    }
-
-    WaitForSingleObject(pi.hProcess, INFINITE);
-    CloseHandle(pi.hProcess);
-
-    input.open(reportFile);
+    std::ifstream input(reportFile);
     if (!input) {
         std::cerr << "Error: Could not open reporter file for reading" << std::endl;
-        return 1;
+        exit(1);
     }
 
     std::string line;
@@ -91,5 +71,24 @@ int main(int argc, char* argv[]) {
     {
         std::cout << line << std::endl;
     }
+
+    input.close();
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Error: Argument exception (must be equal 3)" << std::endl;
+        return 1;
+    }
+
+    std::string command = "Creator.exe " + std::string(argv[1]) + std::string(" ") + std::string(argv[2]);
+    TCHAR commandLine[MAX_PATH];
+    _tcscpy_s(commandLine, MAX_PATH, command.c_str());
+    createProcess(commandLine);
+
+    readEmployeeData(argv[1]);
+
+    generateReport(argv[1]);
+
     return 0;
 }
